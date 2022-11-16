a1c82a82128090bf8db10c7d0b508bfd77a9fafb
Oteng <hagioswilson@gmail.com>
2022-11-16 15:52:33 +0000

    finished with security

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,7 +3,12 @@
 namespace App\Controller;
 
 use App\Message\NewsParser;
+use App\Repository\ArticleRepository;
+use Pagerfanta\Doctrine\ORM\QueryAdapter;
+use Pagerfanta\Pagerfanta;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,12 +18,50 @@ class ArticleController extends AbstractController
     /**
      * @Route("articles")
      */
-    public function index(MessageBusInterface $bus)
+    public function index(ArticleRepository $repository, Request $request): Response
+    {
+        $queryBuilder = $repository->paginatorBuilder();
+
+        $pagerfanta = new Pagerfanta(new QueryAdapter($queryBuilder));
+        $pagerfanta->setMaxPerPage(10);
+        $pagerfanta->setCurrentPage($request->query->get('page', 1));
+
+        return $this->render('articles/index.html.twig', [
+            'pager' => $pagerfanta,
+        ]);
+    }
+
+    /** @Route("/articles/{article_id}/delete", name="delete_article") */
+    public function deleteProduct(int $article_id, ArticleRepository $repository): RedirectResponse
+    {
+        if (!$this->isGranted('ROLE_ADMIN')) {
+            $this->addFlash(
+                'error',
+                'Permission Denied!'
+            );
+
+            return $this->redirectToRoute('app_article_index');
+        }
+
+        $article = $repository->find($article_id);
+
+        $repository->remove($article, true);
+
+        $this->addFlash(
+            'success',
+            'Article deleted successfully'
+        );
+
+        return $this->redirectToRoute('app_article_index');
+    }
+
+    /**
+     * @Route("seed/articles")
+     */
+    public function seed(MessageBusInterface $bus)
     {
         $bus->dispatch(new NewsParser());
 
         return new Response('testing');
     }
-
-
 }
--- src/Repository/ArticleRepository.php
+++ src/Repository/ArticleRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Article;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -39,28 +40,9 @@ class ArticleRepository extends ServiceEntityRepository
         }
     }
 
-//    /**
-//     * @return Article[] Returns an array of Article objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('a')
-//            ->andWhere('a.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('a.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?Article
-//    {
-//        return $this->createQueryBuilder('a')
-//            ->andWhere('a.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    public function paginatorBuilder(): QueryBuilder
+    {
+        return $this->createQueryBuilder('a')
+            ->orderBy('a.id', 'DESC');
+    }
 }
