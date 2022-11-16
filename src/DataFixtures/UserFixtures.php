234d5738861af84a220c92b881e0e1a4240d6503
Oteng <hagioswilson@gmail.com>
2022-11-16 13:16:01 +0000

    added security

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\PasswordHasherInterface;
+
+
+class UserFixtures extends Fixture
+{
+    private PasswordHasherInterface $passwordHasher;
+
+    public function __construct(PasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $user = new User();
+        $user->setEmail('hagioswilson@gmail.com');
+        $user->setName('Emmanuel Oteng Wilson');
+        $user->setRoles(['ROLE_ADMIN']);
+        $user->setPassword($this->passwordHasher->hash('password'));
+        $manager->persist($user);
+        $manager->flush();
+
+        $user1 = new User();
+        $user1->setEmail('hagiose@yahoo.com');
+        $user1->setName('Hagios Wilson');
+        $user1->setRoles(['ROLE_MODERATOR']);
+        $user1->setPassword($this->passwordHasher->hash('password'));
+        $manager->persist($user1);
+        $manager->flush();
+    }
+}
